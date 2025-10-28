d41289a7ab9c6a019dccecbdb8205f0445d9c2aa
Alx2311 <amaquue@gmail.com>
2025-10-27 22:54:57 -0500

    Delete TallerSlearn5.py

--- TallerSlearn5.py
+++
@@ -1,7 +0,0 @@
-#-----Reg. Logistica
-from sklearn.linear_model import LogisticRegression #Importamos el modelo para clasificacion binaria
-x=[[-5],[-4],[-3],[-2],[-1],[1],[2],[3],[4],[5]] #5 datos con 1 caracteristica (5*1)
-y=[0,0,0,0,0,1,1,1,1,1,] #(1 * 10)--> Etiquetas en binario: 0 - y1 +
-model=LogisticRegression() #Declaramos el modelo
-model.fit(x,y)  #Entrenamos con suestrso datos
-print(model.predict([[20]]))  #Realizamos una prediccion
